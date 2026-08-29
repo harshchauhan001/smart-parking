56e9e5d4cdfe2c9c06d953a8573750bc3780cbb3
harshchauhan001 <imharshchauhan2005@gmail.com>
2026-08-29 15:29:26 +0530

    Add production env config for Render backend

--- src/pages/partner/PartnerDashboard.jsx
+++ src/pages/partner/PartnerDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./PartnerDashboard.css";
 
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function PartnerDashboard({ user, onLogout }) {
 
--- src/pages/user/UserDashboard.jsx
+++ src/pages/user/UserDashboard.jsx
@@ -3,7 +3,7 @@ import "./UserDashboard.css";
 
 function UserDashboard({ onLogout }) {
 
-  const API_URL = "http://localhost:3000";
+  const API_URL = import.meta.env.VITE_API_URL;
 
   const getToken = () => {
     return localStorage.getItem("parksmart_token");
